aaa59c214f77b09f2b7f262972ff0ad6c0f7f105
Benton Boychuk-Chorney <benten@Bentons-Air.gv.shawcable.net>
2024-01-13 23:29:36 -0800

    chore: back to ts

--- playground/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import content from "@gaiiaa/content"
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), content({allowDangerousHtml: true})],
-})
--- test/index.test.ts
+++ test/index.test.ts
@@ -6,29 +6,3 @@ describe("packageName", () => {
     expect(true).toBe(true);
   });
 });
-
-const markdown = `
----
-title: Hello World
----
-# Hello World
-this is a test
-## Subtitle
-this is a subtitle
-
-:::code{lang=ts}
-this is a custom component
-:::
-
-## Another subtitle
-this is another subtitle
-`;
-
-const file = await parse(markdown);
-console.log()
-
-const j = toJSX(file.ast, jsx, {
-	custom: {
-		code: "lol",
-	},
-});
